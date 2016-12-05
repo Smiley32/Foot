3095688ffa9a2d908a9b95c41fdae2a45a48c2df
Smiley32 <jeannin.emile@laposte.net>
2016-12-05 15:34:28 +0100

    Codage de Position

--- Position.java
+++ Position.java
@@ -3,14 +3,17 @@ public class Position {
     private int colonne;
     
     public Position() {
-    
+        this.ligne = 0;
+        this.colonne = 0;
     }
     
     public Position(int l, int c) {
-    
+        this.ligne = l;
+        this.colonne = c;
     }
     
     public double distanceAvec(Position p) {
-    
+        return Math.sqrt((p.ligne - this.ligne) * (p.ligne - this.ligne) + (p.colonne - this.colonne) * (p.colonne - this.colonne));
+        // 
     }
 }
