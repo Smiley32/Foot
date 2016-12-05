07dc891f56f4a6af06a3f3d014f3a93ef1de7517
Smiley32 <jeannin.emile@laposte.net>
2016-12-05 15:50:05 +0100

    Codage du Ballon

--- Ballon.java
+++ Ballon.java
@@ -3,5 +3,42 @@ public class Ballon {
     private Direction dir;
     private Playable derniereFrappe;
     
-    public Ballon()
+    public Ballon() {
+        this.pos = new Position();
+        this.dir = new Direction();
+    }
+    
+    public Ballon(Position p) {
+        this.pos = p;
+        this.dir = new Direction();
+    }
+    
+    public Ballon(Position p, Direction d) {
+        this.pos = p;
+        this.dir = d;
+    }
+    
+    public void setPosition(Position p) {
+        this.pos = p;
+    }
+    
+    public Position getPosition() {
+        return this.pos;
+    }
+    
+    public void setDirection(Direction d) {
+        this.dir = d;
+    }
+    
+    public Direction getDirection() {
+        return this.dir;
+    }
+    
+    public void setDerniereFrappe(Playable p) {
+        this.derniereFrappe = p;
+    }
+    
+    public Playable getDerniereFrappe() {
+        return this.derniereFrappe;
+    }
 }
