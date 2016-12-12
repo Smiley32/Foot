7b0e3c761f73a5a0c8a5642700d6747ae22555a7
PANDA*NBK <theomottet@gmail.com>
2016-12-12 16:39:37 +0100

    aaa

--- Milieu.java
+++ Milieu.java
@@ -11,8 +11,8 @@ public class Milieu extends Joueur {
     public Direction deplacement() { 
         if(pos.distanceAvec(this.t.getBallon().getPosition()) < 3){
             return pos.seDirigerVers(positionInitiale);
-        } else if (pos.distanceAvec(positionInitiale) == 0) {
-             return new Direction(-1,0);
+        } else if (pos.getLigne() == positionInitiale.getLigne()) {
+             return new Direction(Outils.rand(-1,1),0);
         } else {
             return new Direction(Outils.rand(-1,1), Outils.rand(-1, 1));
         } 
